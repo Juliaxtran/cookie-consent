37b155a7de9e67d71047bbe878b71f4df66d1329
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-20 18:50:37 -0500

    Close modal button

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const modal = document.getElementById('modal')
+const closeBtn = document.getElementById('modal-close-btn')
 
 setTimeout(() => {
     modal.style.display = 'inline'
 }, 1500)
+
+
+
+closeBtn.addEventListener('click', () => {
+   modal.style.display = 'none'
+} );
